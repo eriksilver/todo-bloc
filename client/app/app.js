ecc8c695fba0a4d0eb0d41f46071b1e73532b1e5
eriksilver <eriksilver@users.noreply.github.com>
2015-04-03 11:02:46 -0500

    Start over - app working now

--- client/app/app.js
+++ client/app/app.js
@@ -5,33 +5,12 @@ angular.module('blocitoffApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap',
-  'firebase'
-  ])
-
+  'ui.bootstrap'
+])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
-  })
+  });
 
-  .controller("SampleCtrl", function($scope, $firebaseArray) {
-    var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com/");
- 
-    //Firebase has a method called $asArray() that can be used to sync a Firebase database 
-    //with an Angular model like an array of tasks. Set the $scope array holding your tasks 
-    //to a Firebase object that calls $asArray().
-
-
-     // create a synchronized array
-    $scope.messages = $firebaseArray(ref);
-    // add new items to the array
-    // the message is automatically added to Firebase!
-    $scope.addMessage = function() {
-      $scope.messages.$add({
-        text: $scope.newMessageText
-      });
-    };
-  })
-});
