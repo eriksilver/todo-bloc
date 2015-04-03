f23cc7c7b5fc69197d41f60d96cbe4f434d7e592
eriksilver <eriksilver@users.noreply.github.com>
2015-04-03 11:25:58 -0500

    Firebase injected, messages array demo working

--- client/app/app.js
+++ client/app/app.js
@@ -5,12 +5,29 @@ angular.module('blocitoffApp', [
   'ngResource',
   'ngSanitize',
   'ui.router',
-  'ui.bootstrap'
-])
+  'ui.bootstrap',
+  'firebase'
+  ])
+
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+  })
+
+  //controller for firebase messages demo
+  .controller("SampleCtrl", function($scope, $firebaseArray) {
+    var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com/messages");
+    // create a synchronized array
+    $scope.messages = $firebaseArray(ref);
+    // add new items to the array
+    // the message is automatically added to Firebase!
+    $scope.addMessage = function() {
+      $scope.messages.$add({
+        text: $scope.newMessageText
+      });
+    };
+    // click on `index.html` above to see $remove() and $save() in action
   });
 
