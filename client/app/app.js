7ee145fe0a039f8fd95109ce0deadcb99222b3fc
eriksilver <eriksilver@users.noreply.github.com>
2015-04-07 07:24:44 -0500

    tasks wired up and working and home route  working too

--- client/app/app.js
+++ client/app/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+console.log("appjs before module declared");
 angular.module('blocitoffApp', [
   'ngCookies',
   'ngResource',
@@ -14,24 +15,6 @@ angular.module('blocitoffApp', [
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
-  })
-
-  //controller similar to firebase messages demo
-  .controller("TaskCtrl", function($scope, $firebaseArray) {
-    var taskData = new Firebase("https://dazzling-torch-1941.firebaseio.com/tasks");
-    // create a synchronized array
-    $scope.tasks = $firebaseArray(taskData);
-
-    //test for tasks array
-    // $scope.tasks.$add({"text": "the one we just added"});
-    
-    // add new items to the array
-    // the task is automatically added to Firebase!
-    $scope.addTask = function() {
-      $scope.tasks.$add({
-        text: $scope.newTaskText
-      });
-    };
-    // click on `index.html` above to see $remove() and $save() in action
   });
 
+console.log("appjs after module delclared");
--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,5 +1,23 @@
 app = angular.module('blocitoffApp');
 
-app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
-
+app.controller('HomeCtrl', ['$scope', '$http', '$firebaseArray', function($scope, $http, $firebaseArray) {
+    
+    console.log("home.controller.js start");
+    
+    //controller similar to firebase messages demo
+    var taskData = new Firebase("https://dazzling-torch-1941.firebaseio.com/tasks");
+    // create a synchronized array
+    $scope.tasks = $firebaseArray(taskData);
+  
+    //test for tasks array
+    //$scope.tasks.$add({"text": "first task added"});
+    
+    // add new items to the array
+    // the task is automatically added to Firebase!
+    $scope.addTask = function() {
+      $scope.tasks.$add({
+        text: $scope.newTaskText
+      });
+    };
+    console.log("home.controller.js finished");
 }]);
