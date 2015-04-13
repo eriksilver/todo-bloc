e3229b8ad49f39e266c60e0d58d82ba470ef343c
eriksilver <eriksilver@users.noreply.github.com>
2015-04-13 17:48:06 -0500

    progress on 3 way binding example

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,5 +1,21 @@
 app = angular.module('blocitoffApp');
 
+app.controller("SampleCtrl", function($scope, $firebaseObject, $firebaseArray) {
+  var ref = new Firebase("https://dazzling-torch-1941.firebaseio.com/data");
+  var itemsRef = new Firebase("https://dazzling-torch-1941.firebaseio.com/dataItems");
+
+  // download the data into a local object
+  var syncObject = $firebaseObject(ref);
+  // var syncArray = $firebaseArray(itemsRef);
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+  syncObject.$bindTo($scope, "data");
+  // syncArray.$bindTo($scope, "dataItems");
+  $scope.dataItems = $firebaseArray(itemsRef);
+
+
+});
+
 app.controller('HomeCtrl', ['$scope', '$http', '$firebaseArray', '$firebaseObject', function($scope, $http, $firebaseArray, $firebaseObject) {
     
     console.log("home.controller.js start");
