57377189165704118f3155bc18df713e9142212e
eriksilver <eriksilver@users.noreply.github.com>
2015-05-14 13:47:44 -0500

    task timer working and code cleanup

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,4 +1,4 @@
-//'use strict';
+  //'use strict';
 
 app = angular.module('blocitoffApp');
 
@@ -19,16 +19,11 @@ app.controller('HomeCtrl', ['$scope', '$http', '$firebaseArray', '$timeout', fun
         text: null,
         status: false,
         priority: 'medium',
-        createdAt: null,
         expiresAt: null // This is set in $scope.addTask
     };
 
     //function reset input form
     $scope.reset = function() {
-      // if (addForm) {
-      //   addForm.$setPristine();
-      //   addForm.$setUntouched();
-      // }
       $scope.newTask = {};
     };
     
@@ -39,106 +34,87 @@ app.controller('HomeCtrl', ['$scope', '$http', '$firebaseArray', '$timeout', fun
         //add new task to Firebase
         var newTask = $scope.newTask;
         
-        //Set task timer as expiration date
-        // newTask.createdAt = ;
-        $scope.newTask.expiresAt = Date.now() + 3000;
+        //Set task expiration with current time 
+        //Timer is set with (Date.now()) + milliseconds or days
+        var days = 24 * 60 * 60 * 1000 //hours mins secs millisecs
+        $scope.newTask.expiresAt = Date.now() + days;
         console.log("expiresAt stamp:", newTask.expiresAt);
-        //console.log("true or false", 8 - 9 < 0);
 
         //add newTask object to Firebase array using Firebase $add method
         $scope.tasks.$add($scope.newTask);
 
-        //call the reset function to clear entry form
+        //call the reset function to clear newTask object, which clears entry form
         $scope.reset();
-
-        console.log("$scope.tasks.expiresAt:", $scope.tasks.expiresAt);
     };
+
+
+     //console log a recall of array data
+    taskData.on("child_added", function(snapshot) {
+      var consoleTask = snapshot.val();
+      // console.log("Text: " + consoleTask.text);
+      // console.log("Status: " + consoleTask.status);
+      // console.log("priority: " + consoleTask.priority);
+      // console.log("createdAt: " + consoleTask.createdAt);
+      // console.log("expiresAt: " + consoleTask.expiresAt);
+      // console.log("-----------")
+      console.dir(consoleTask);
+    });
+
+    // using ng init
+    // call ng-init in list items; every time items listed, call checkTaskExpired
+    // checkTaskExpired uses getRecord and id and checks each list items
+      //make task expire after certain time
+    $scope.checkTaskExpiration = function(taskId) {
+      console.log("START checkTaskExpiration");
+      var taskRecord = $scope.tasks.$getRecord(taskId);
+      var now = Date.now();
+
+      if (taskRecord.expiresAt - now < 0) {
+        taskRecord.status = true;
+        $scope.tasks.$save(taskRecord);
+      };
+      console.log('finished checkTaskExpiration');
+
+      // var task = tasks.$getRecord(taskId);
+      // var today = new Date();
+      // var now = today.getTime();
+      // var days = 24 * 60 * 60 * 1000 //hours mins secs millisecs
+
+      // if(task.state == "active" && (now - task.dateAdded) >= (7 * days)) {
+      //   task.state = "expired";
+      //   tasks.$save(task);
+    };
+
+
+    console.log("home.controller.js finished");
+}]);
+
+
 
     // $scope.checkTaskExpired = function(task) {
     //     $scope.updateExpiredStatus();
     //     return task.status == true && task.expiresAt - Date.now() > 0 && false
     // };
 
-    $scope.updateExpiredStatus = function() {
-        console.log("running updateExpiredStatus function");
-        //var isExpired = $scope.task.expiresAt - Date.now() < 0;
-        // console.log("var isExpired:", isExpired );
+   //  $scope.updateExpiredStatus = function() {
+   //      console.log("running updateExpiredStatus function");
+   //      //var isExpired = $scope.task.expiresAt - Date.now() < 0;
+   //      // console.log("var isExpired:", isExpired );
 
-        // if (isExpired) {
-        //     $scope.task.isExpired = true;
-        // }
-    };
+   //      // if (isExpired) {
+   //      //     $scope.task.isExpired = true;
+   //      // }
+   //  };
 
-   $scope.updateExpiredStatus();
+   // $scope.updateExpiredStatus();
 
-	// // Given a DataSnapshot containing a child "fred" and a child "wilma", this callback
-	// // function will be called twice
-	// taskData.forEach(function(childSnapshot) {
-	//   // key will be "fred" the first time and "wilma" the second time
-	//   var key = childSnapshot.key();
-	//   console.log("key:");
-	//   // childData will be the actual contents of the child
-	//   var childData = childSnapshot.val();
-	//   console.log("childData:");
-	// });
-
-
-     //console log a recall of array data
-    taskData.on("child_added", function(snapshot) {
-      var consoleTask = snapshot.val();
-      console.log("Text: " + consoleTask.text);
-      console.log("Status: " + consoleTask.status);
-      console.log("priority: " + consoleTask.priority);
-      console.log("createdAt: " + consoleTask.createdAt);
-      console.log("expiresAt: " + consoleTask.expiresAt);
-      console.log("-----------")
-      console.dir(consoleTask);
-    });
-
-    //Expired task manager function
-    //Write a function that takes in objects
-
-    //One function that reviews all tasks
-    //need to adapt to iterate over Firebase array
-    var manageExpiredTasks = function(tasks) {
-      console.log("manage tasks running",tasks.length);
-      for (var i = 0; i < tasks.length; i++) {
-        tasks[i];
-        console.log("Here is tasks[i]:", tasks[i]);
-        console.log("Here is tasks[i].expiresAt:",tasks[i].expiresAt);
-        isTaskExpired();
-      };
-    };
-
-    manageExpiredTasks($scope.tasks);
-
-    //One function to determine if task is expired or not
-    var isTaskExpired = function(task) {
-      if (task.expiresAt - Date.now() > 0) {
-        return false //task is NOT expired
-      } 
-      else {
-        return true //task is expired
-      }
-    };
-
-    //setup variables to extract data from Firebase
-    //"once" is like a watch method that only acts on an update of a value
-    var taskObjects;
-    taskData.once('value', function(dataSnapshot) {
-      console.log("dataSnapshot function");
-  		// store dataSnapshot for use in below examples.
-  		taskObjects = dataSnapshot;
-
-      var savedData = taskObjects.val();
-      console.dir(savedData);
-	});
-
-    // using ng init
-    // call ng-init in list items; every time items listed, call checkTaskExpired
-    // checkTaskExpired uses getRecord and id and checks each list items
-    
-
-
-    console.log("home.controller.js finished");
-}]);
+  // // Given a DataSnapshot containing a child "fred" and a child "wilma", this callback
+  // // function will be called twice
+  // taskData.forEach(function(childSnapshot) {
+  //   // key will be "fred" the first time and "wilma" the second time
+  //   var key = childSnapshot.key();
+  //   console.log("key:");
+  //   // childData will be the actual contents of the child
+  //   var childData = childSnapshot.val();
+  //   console.log("childData:");
+  // });
