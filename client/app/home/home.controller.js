832d1376846f1731ae2d5ef198193185a9674188
eriksilver <eriksilver@users.noreply.github.com>
2015-05-15 14:17:27 -0500

    added  service

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -1,9 +1,20 @@
   //'use strict';
 
+//app = angular.module('blocitoffApp', []); //this throws injector error
 app = angular.module('blocitoffApp');
 
+app.config(['$logProvider', function($logProvider){
+    $logProvider.debugEnabled(false);
+}]);
+
 //tasks controller
-app.controller('HomeCtrl', ['$scope', '$http', '$firebaseArray', '$timeout', function($scope, $http, $firebaseArray, $timeout) {
+app.controller('HomeCtrl', [
+  '$scope',
+  '$http',
+  '$firebaseArray',
+  '$timeout',
+  '$log',
+  function($scope, $http, $firebaseArray, $timeout, $log) {
         
     //database for current task Data
     var taskData = new Firebase("https://dazzling-torch-1941.firebaseio.com/tasks");
@@ -12,34 +23,29 @@ app.controller('HomeCtrl', ['$scope', '$http', '$firebaseArray', '$timeout', fun
     $scope.tasks = $firebaseArray(taskData);
 
     // define task properties
-    $scope.newTask = {
+    $scope.newTaskDefaults = {
         text: null,
         status: false,
         priority: 'medium',
         expiresAt: null // This is set in $scope.addTask
     };
+    $scope.newTask = angular.copy($scope.newTaskDefaults);
     //console.log("newTask completed");
 
     //function reset input form
     $scope.reset = function() {
-      $scope.newTask = {};
+      $scope.newTask = angular.copy($scope.newTaskDefaults);
     };
     
     // addTask function called when new task is entered
     $scope.addTask = function() {
-      console.log("addTask Called - result - $scope.newTask:", $scope.newTask);
-
-        //copy $scope.newTask to local variable newTask
-        var newTask = $scope.newTask;
+      $log.debug("addTask Called - result - $scope.newTask:", $scope.newTask);
         
         //Set task expiration with current time 
         //Timer is set with (Date.now()) + milliseconds or days
         var days = 24 * 60 * 60 * 1000 //hours mins secs millisecs
         $scope.newTask.expiresAt = Date.now() + days;
-        console.log("expiresAt stamp:", newTask.expiresAt);
-
-        $scope.newTask.status = false;
-        $scope.newTask.priority = 'medium';
+        $log.debug("expiresAt stamp:", $scope.newTask.expiresAt);
 
         //add newTask object to Firebase array using Firebase $add method
         $scope.tasks.$add($scope.newTask);
@@ -48,24 +54,12 @@ app.controller('HomeCtrl', ['$scope', '$http', '$firebaseArray', '$timeout', fun
         $scope.reset();
     };
 
-     //console log a recall of array data
-    taskData.on("child_added", function(snapshot) {
-      var consoleTask = snapshot.val();
-      // console.log("Text: " + consoleTask.text);
-      // console.log("Status: " + consoleTask.status);
-      // console.log("priority: " + consoleTask.priority);
-      // console.log("createdAt: " + consoleTask.createdAt);
-      // console.log("expiresAt: " + consoleTask.expiresAt);
-      // console.log("-----------")
-      console.dir(consoleTask);
-    });
-
     // using ng init
     // call ng-init in list items; every time items listed, call checkTaskExpired
     // checkTaskExpired uses getRecord and id and checks each list items
     //make task expire after certain time
     $scope.checkTaskExpiration = function(taskId) {
-      //console.log("START checkTaskExpiration");
+      $log.debug("START checkTaskExpiration");
       var taskRecord = $scope.tasks.$getRecord(taskId);
       var now = Date.now();
 
